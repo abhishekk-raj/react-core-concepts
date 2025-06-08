e0be869302a5dc8a5a898d3f836780678d4e7708
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-08 19:51:52 +0530

    Initial commit with deal with array state

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReactHooks from "./components/hooks/ReactHooks.tsx";
+
+function App() {
+  return (
+    <>
+      <ReactHooks/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hooks/ReactHooks.tsx
@@ -0,0 +1,11 @@
+import PlayWithUseState from "./PlayWithUseState.tsx";
+
+const ReactHooks = () => {
+  return (
+    <>
+      <PlayWithUseState/>
+    </>
+  )
+}
+
+export default ReactHooks;
