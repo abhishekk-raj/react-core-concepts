afe0eaac2ad473d818ec3b8e0f86426d1bc1355c
Abhishek Raj <abhishekk.raj@outlook.com>
2025-06-09 20:29:10 +0530

    Use Layout Effect

--- src/components/hooks/UseEffectUnmountCleanup.tsx
+++ src/components/hooks/UseEffectUnmountCleanup.tsx
@@ -1,8 +1,18 @@
-import {useEffect} from "react";
+import {useEffect, useLayoutEffect} from "react";
 
 const UseEffectUnmountCleanup = () => {
 
+  /*
+  * We use LayoutEffect if we need to measure or mutate the DOM
+  * before the browser paitns it
+  * */
+  useLayoutEffect(() => {
+    const height = window.innerHeight;
+    console.log("Window Height :: ", height);
+  }, []);
+
   useEffect(() => {
+    console.log('UseEffect Called');
     const handler = () => console.log("Window Resized");
     window.addEventListener("resize", handler);
 
